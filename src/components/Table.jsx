95116f019b6b545385da30805bcec6592e84b9e8
Ivan <belzee92@gmail.com>
2019-05-04 22:14:34 -0400

    add exclude fields logic to table cmp

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,28 +5,33 @@ import Button from "./Button";
 import Alert from "./Alert";
 
 /**
- * @description render all Object values in a <td> tag
+ * @description render the table body
  * @param {Object} obj
+ * @param {Array} exclude
  */
-const renderValues = obj =>
-  Object.keys(obj).map((key, index) => <td key={index}>{obj[key]}</td>);
+const renderValues = (obj, exclude) =>
+  Object.keys(obj).map((key, index) =>
+    !exclude.includes(key) ? <td key={index}>{obj[key]}</td> : null
+  );
 
 /**
- * @description render all Object's keys in a <th> tag
+ * @description render the table header
  * @param {Object} obj
+ * @param {Array} exclude
  */
-const renderKeys = obj =>
-  Object.keys(obj).map((key, index) => <th key={index}>{key}</th>);
+const renderKeys = (obj, exclude) =>
+  Object.keys(obj).map((key, index) =>
+    !exclude.includes(key) ? <th key={index}>{key}</th> : null
+  );
 
 const Table = props => {
-  const { type, data, onDeleteItem } = props;
-  console.log(data);
+  const { type, data, onDeleteItem, excludeFields } = props;
   return data.length ? (
     <table className={`table table-${type}`}>
       <thead>
         <tr>
           <th scope="col">#</th>
-          {renderKeys(data[0])}
+          {renderKeys(data[0], excludeFields)}
           <th>Actions</th>
         </tr>
       </thead>
@@ -34,7 +39,7 @@ const Table = props => {
         {data.map((item, index) => (
           <tr key={item.id}>
             <td style={{ fontWeight: "bold" }}>{index + 1}</td>
-            {renderValues(item)}
+            {renderValues(item, excludeFields)}
             <td>
               <Button className="mr-1" type="warning" size="sm" outline>
                 Edit
@@ -67,12 +72,14 @@ Table.propTypes = {
     "hover",
     "sm"
   ]),
-  data: PropTypes.arrayOf(PropTypes.object)
+  data: PropTypes.arrayOf(PropTypes.object),
+  excludeFields: PropTypes.arrayOf(PropTypes.string)
 };
 
 Table.defaultProps = {
   type: "",
-  data: []
+  data: [],
+  excludeFields: ["id"]
 };
 
 export default Table;
